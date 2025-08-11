34313182b94f0b199f2c4cf4668a7851c4e6761c
Sugam Thapa <tsugam74@gmail.com>
2025-08-11 21:22:54 +0545

    change debug in settings

--- NclexProject/NclexProject/settings.py
+++ NclexProject/NclexProject/settings.py
@@ -28,9 +28,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4)mkmp(7_nrie6r&991umw-q6l3&iq0mp_%5-aaiiw!w)mhn@6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+# DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = False
 
 ALLOWED_HOSTS = ['quizproject-0r93.onrender.com', 'localhost', '127.0.0.1']
 
